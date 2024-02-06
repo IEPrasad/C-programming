ce3e479171478616a586f2d0af3fcaec96d6d19c
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-06 08:11:06 +0530

    Add files via upload

---
+++ 02_C_05.c
@@ -0,0 +1,9 @@
+---------- Structure of C Program ---------
+
+
+  1. Documentation Section
+  2. Link Section
+  3. Definition Section
+  4. Global deduction Section
+  5. Main Section (compulsory)
+  6. Sub Program Section
