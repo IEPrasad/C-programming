054c220afe9f1619f79e8d7eae5adb4b74feb129
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-06 08:07:20 +0530

    Add files via upload

---
+++ 01_C_04.c
@@ -0,0 +1,36 @@
+    
+    // --------- Welcome to Programming with C -----
+
+  Features of C
+    1. High level language
+    2. Easily learn this language having just 
+       only 32 keywords.
+    3. It is a core language 
+    4. Portalble 
+    5. It is have building functions and operators
+    6. It is a structured language
+    7. It supports use of pointers 
+    (You can direct interact with the memory)
+    8. It is Extentible language
+    (You can add your own functions to C librarie)
+    9. It support features of dynamic memory location
+    (free the memory at any time by calling)
+    10. It supports modular Programming
+    (or rather than writing in a separate point we 
+    can write down here also it is structured)
+    11. It is case sensitive language
+    12. It use to develop and embedded systems
+    
+
+    1. It is platform depended language
+    ** (when you are writing a program in widows 
+    compliler create object file. that object file
+    you cannot run on the Linux or Max .. )
+
+    (windows files extension = .exe)
+    (for mac I think extension is = .dmg)
+
+
+
+
+    -------------------------------------------------
