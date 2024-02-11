7ff6c2fa5543c202af8a5d2a51a02a51b8114761
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-11 17:47:02 +0530

    Add files via upload

---
+++ 01-hello-world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!");
+  
+  return 0;
+}
+
+// Remember to put the Double quatation marks 
+// because single quatation is not allowed.
+// and also `` are not allowed. 
