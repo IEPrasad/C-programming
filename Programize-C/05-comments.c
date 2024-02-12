7cd640e30744c19f01998598056a303f8258b9b3
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-12 14:29:01 +0530

    Add files via upload

---
+++ Programize-C/05-comments.c
@@ -0,0 +1,54 @@
+// #include <stdio.h>
+
+// int main() {
+
+//   // create a variable 
+//   int data = 34;
+
+//   // print the value of data variable
+//   printf("Data = %d", data);
+
+//   return 0;
+// }
+
+
+    // Prevent Executing  Code Using Comments 
+
+/*
+#include <stdio.h>
+
+int main() {
+
+  int age;
+  double height;
+
+  printf("Enter the age: ");
+  scanf("%d", &age);
+
+  printf("Enter the height: ");
+  scanf("\n%lf", &height);
+
+  printf("Age = %d", age);
+  printf("\nHeight = %.2lfcm", height);
+
+  return 0;
+}
+*/
+
+      // Multiline Comments in C programming
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
