9b5b51a900879db8a344d9b2f396a6ccb34e5197
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-12 15:51:33 +0530

    Add files via upload

---
+++ Programize-C/06-operators.c
@@ -0,0 +1,116 @@
+
+  // ** Arithmetic Operators
+
+  // +    Addition 
+  // -    Substraction 
+  // *    Multiplication 
+  // /    Division 
+  // %    Remainder
+  // ++   Increment (increments by 1)
+  // --   Decrement (decrements by 1)
+
+// -------- Addition Operator
+
+// #include <stdio.h>
+
+// int main() {
+//   int x = 12;
+
+//   int result = x + 8;
+
+//   printf("%d", result);
+
+//   return 0;
+// }
+
+
+
+// #include <stdio.h>
+
+// int main() {
+//   double x = 12.57;
+//   int y = 8;
+
+//   double result = x + y;
+
+//   printf("%.2lf", result);
+
+//   return 0;
+// }
+
+
+
+// ----------- Division Operator
+
+// #include <stdio.h>
+
+// int main() {
+
+//   double x = 12.00;
+
+//   double result = x / 8.00;
+
+//   printf("%.2lf", result);
+
+//   return 0;
+// }
+
+
+// ------------- Remainder Operator
+
+
+// #include <stdio.h>
+
+// int main() {
+
+//   int x = 12.00;
+
+//   int result = x % 8;
+
+//   printf("%d", result);
+
+//   return 0;
+// }
+
+
+// ------------- Increment and Decrement Operator
+
+
+// #include <stdio.h>
+
+// int main() {
+
+//   int x = 12;
+
+//   // printf("%d", ++x);
+//     // >> 13
+//   printf("%d", --x);
+//     // >> 11
+
+//     // ** we can use that x++ or x-- like this 
+//     // or we can use that like ^^^^
+
+//   return 0;
+// }
+
+
+// --------------- Multiple Operators
+
+#include <stdio.h>
+
+int main() {
+
+  int x = 4 / 2 + 6 * 5 - 1;
+
+  printf("%d", x);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
