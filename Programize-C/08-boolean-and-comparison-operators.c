8fde6a183a8173ea17d7173cde37862b4feafe8a
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-13 17:17:21 +0530

    Add files via upload

---
+++ Programize-C/08-boolean-and-comparison-operators.c
@@ -0,0 +1,225 @@
+/*
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+  bool value1 = true;
+  bool value2 = false;
+
+  printf("%d", value1);
+  printf(" %d", value2);
+
+  return 0;
+}
+*/
+/*
+      Comparison Operators
+
+  >   Greater than
+  <   Less than 
+  ==  Equal to 
+  >=  Greater than or equal to 
+  <=  Less than or equal to 
+  !=  Not equal to 
+
+*/
+/* 
+
+// ---------- Greater than Operator
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+  bool value = (12 > 9);
+
+  printf("%d ", value);
+      // >> 1
+
+  return 0;
+}
+
+*/
+
+//  ----------- Less than Operator 
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main() {
+
+//   bool value = (5 < 9);
+
+//   printf("%d ", value);
+//       // >> 1
+
+//   return 0;
+// }
+
+
+// -------- Equal to Operator
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main() {
+
+//   bool value = (5 == 9);
+
+//   printf("%d ", value);
+//       // >> 0
+
+//   return 0;
+// }
+
+
+// ------------- Not Equal to Operator
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main() {
+
+//   bool value = (5 != 9);
+
+//   printf("%d ", value);
+//       // >> 1
+
+//   return 0;
+// }
+
+// ------------ Greater than or Equal Operator
+
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main() {
+
+//   bool value = (5 >= 9);
+
+//   printf("%d ", value);
+//       // >> 0
+
+//   return 0;
+// }
+
+//  --------------- Less than or Equal Operator
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main() {
+
+//   bool value = (5 <= 9);
+
+//   printf("%d ", value);
+//       // >> 1
+
+//   return 0;
+// }
+
+
+// ----------------------------------------------------
+
+      // Comparison between variables
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main() {
+
+//   int num1 = 9;
+//   int num2 = 6;
+
+//   bool value = num1 >num2;
+
+//   printf("%d ", value);
+//       // >> 1
+
+//   return 0;
+// }
+      
+
+// ------------------------------------------------------
+
+        // Logical Operators
+
+
+    // &&      AND 
+    // ||      OR
+    // !       NOT       
+
+
+// ------- AND Operator
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main() {
+  
+//   int age = 18;
+//   double height = 6.3;
+
+//   bool result = (age >= 18) && (height > 6);
+//   printf("%d", result);
+//       // >> 1
+
+//   return 0;
+// }
+
+// --------- OR Operator
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main() {
+  
+//   int age = 18;
+//   double height = 6.3;
+
+//   bool result = (age >= 18) || (height > 6);
+//   printf("%d", result);
+//       // >> 1
+
+//   return 0;
+// }
+
+
+// ---------------- NOT Operator
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main() {
+  
+//   int age = 15;
+
+//   bool result = !(age >= 18);
+//   printf("%d", result);
+//       // >> 1
+
+//   return 0;
+// }
+
+// ------------------------------------------------
+
+
+Question.
+  Which of the following code returns false ?
+
+  A. 9 >= 9
+  B. 9 > 9
+  C. 9 <= 9
+  D. 9 == 9
+
+  As I know:
+  the answer will  B
+
+
+
+
+
