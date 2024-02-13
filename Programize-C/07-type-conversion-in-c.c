a66500e90fb3f8bc35c2d1574c4e34b6c13d918e
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-13 16:14:16 +0530

    Add files via upload

---
+++ Programize-C/07-type-conversion-in-c.c
@@ -0,0 +1,143 @@
+
+/*
+#include <stdio.h>
+
+int main() {
+
+  // char a = '5';
+  // int b = 9;
+
+  // int result = a + b;
+
+  // printf("%d", result);
+      // >> 62
+// -------------------
+
+  // double a = 5.67;
+  // int b = 9;
+
+  // int result = a + b;
+  
+  // printf("%d", result);
+  //     // >> 14
+// ------------------------
+
+  int a = 5.67;
+  int b = 9;
+
+  int result = a + b;
+  
+  printf("%d", result);
+  return 0;
+}
+*/
+
+        // Explict Type Conversion -------------
+
+
+# include <stdio.h>
+
+// int main() {
+
+//   double a = 5.67;
+//   int b = 9;
+
+//   double result = (int)a + b;
+
+//   printf("%lf", result);
+//       // >> 14.000000
+//   return 0;
+
+
+// int main() {
+
+//   double a = 5.67;
+//   int b = 9;
+
+//   int result = (int)a + b;
+
+//   printf("%d", result);
+//       // >> 14
+//   return 0;
+// }
+
+// -------------------------
+
+// int main() {
+
+//   int a = 9;
+//   int b = 2;
+
+//   double result = a / b;
+
+//   printf("%lf", result);
+//       // >> 4.000000
+//   return 0;
+// }
+
+      // vvvvvvvvv
+
+// int main() {
+
+//   int a = 9;
+//   int b = 2;
+
+//   double result = (double)a / b;
+
+//   printf("%lf", result);
+//       // >> 4.500000
+//   return 0;
+  
+// }
+
+
+
+// ------- Question 
+
+// What is the value of result in the following statement ?
+
+
+#include <stdio.h>
+
+int main() {
+
+  int result = '8' + 12;
+  printf("%d", result);
+      // >> 68
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
