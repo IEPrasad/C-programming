5b9d69a72487343ec0c0e2d432a44d05f1696f14
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-14 08:49:23 +0530

    Add files via upload

---
+++ Programize-C/10-ternary-operator.c
@@ -0,0 +1,139 @@
+// Ternary Operator in C
+
+// test_condition ? expression1 : expression2;
+
+// #include <stdio.h>
+
+// int main() {
+
+//   int age = 15;
+
+//   (age >= 18) ? printf("You can vote") : printf("You cannot vote");
+
+//   return 0;
+// }
+
+
+
+// -----------------
+
+// #include <stdio.h>
+
+// int main() {
+
+//   char operator = '+';
+
+//   int num1 = 8;
+//   int num2 = 7;
+
+//   int result = (operator == '+') ? (num1 + num2) : (num1 - num2);
+//   printf("%d", result);
+
+
+//   return 0;
+// }
+
+// -------------------------------------------
+
+// Ternary Operator      
+
+
+// #include <stdio.h>
+
+// int main() {
+
+//   int age = 15;
+
+//   (age >= 18) ?
+//     printf("You can vote") :
+//     printf("You cannot vote");
+
+//   return 0;
+// }
+
+
+// let's see how to do this with fi else 
+
+// #include <stdio.h>
+
+// int main() {
+
+//   int age = 15;
+
+//   if (age >= 18) {
+//     printf("You can vote");
+//   } else {
+//     printf("You cannot vote");
+//   }
+
+//   return 0;
+// }
+
+
+// ----------------------------------------------
+
+
+// Programming Task
+
+
+// Can you create a program to check whether a number is odd or even?
+
+//   ** Use a ternary operator to check if the number is odd or even
+
+//   ** If number is odd, print "The number is Odd"
+
+//   ** If number is even, print "The number is Even"
+
+
+// #include <stdio.h>
+
+// int main() {
+//   int number = 5;
+
+//   (number % 2 == 0) ?
+//     printf("The number is Even"):
+//     printf("The number is odd");
+
+
+//   return 0;
+// }
+
+
+
+// ---------------------------------
+
+
+Q. What is the correct ternary equivalent of the 
+   following if...else statement?
+
+   if (5 > 3) {
+    result = 5;
+   } else {
+    result = 3;
+   }
+
+A. result = 5 > 3 ? 3 : 5;
+B. 5 > 3 ? result = 5 : 3;
+C. result = 5 > 3 ? 5 : 3;
+D. 5 > 3 ? result = 3 : 5;
+
+
+** As I know the answer is C
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
