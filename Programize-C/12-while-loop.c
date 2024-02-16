0b4f46af9a95bc521fd5218ee59a7134e7f1df48
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-16 16:16:48 +0530

    Add files via upload

---
+++ Programize-C/12-while-loop.c
@@ -0,0 +1,59 @@
+// #include <stdio.h>
+
+// int main() {
+//   int count = 1;
+
+//   while (count < 5) {
+//     printf("While loop in C\n");
+//     printf("Count = %d\n", count);
+//     count += 1;
+//   }
+
+//   return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main() {
+
+//   int number;
+//   printf("Enter the number: ");
+//   scanf("%d", &number);
+
+//   int count = 1;
+
+//   while (count <= 10) {
+//     int product = number * count;
+//     printf("%d * %d = %d\n  ", number, count,   product);
+//     count += 1;
+//   }
+
+
+//   return 0;
+// }
+
+
+      // Do.... while loop 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
