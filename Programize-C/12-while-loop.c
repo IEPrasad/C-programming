4f8358807f43b3147618181df88168ef624b8d53
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-16 16:38:03 +0530

    Update 12-while-loop.c

--- Programize-C/12-while-loop.c
+++ Programize-C/12-while-loop.c
@@ -41,19 +41,19 @@
 //   // body of loop
 // } while (condition);
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
+// int main() {
 
-  int count = 1;
+//   int count = 1;
 
-  do {
-    printf("%d\n", count);
-    count += 1;
-  } while (count < 5);
+//   do {
+//     printf("%d\n", count);
+//     count += 1;
+//   } while (count < 5);
 
-  return 0;
-}
+//   return 0;
+// }
 
 /*
 // ----------------------------
@@ -79,12 +79,46 @@ while (count < 5) {
 */
 
 
+// Programming Task
+
+// Can you use the while loop to print the 
+// multiplication table for the given number.
+// print the number from 10 to 1. So the output 
+// would 
+// be something like this.
+
+// 9 * 10 = 90
+// 9 * 9 = 81
+// 9 * 8 = 72
+// 9 * 7 = 63
+
+// ---
+// ---
+
+// 9 * 1 = 9
+
+// ----------------------------------
 
 
 
 
+  #include <stdio.h>
+  
+  int main() {
 
+    int count = 10;
+    int number;
+    printf("Enter number: ");
+    scanf("%d", &number);
 
+    while (count >= 1) {
+      int result = number * count;
+      printf("%d * %d = %d\n  ", number, count, result);
+      count -= 1;
+    }
+
+    return 0;
+  }
 
 
 
