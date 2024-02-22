e18ec41543979e1c1a6989c68d1abed742163a8e
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-22 23:55:29 +0530

    Add files via upload

---
+++ Programize-C/15-functions.c
@@ -0,0 +1,140 @@
+// Functions in C 
+
+// ** a fucntion to create a circle 
+// ** a function to create a rectangle 
+// ** a function to color the shape 
+
+// ** when we use the void don't need to use the "return
+// statement"
+
+// #include <stdio.h>
+
+// void greet() {
+//   printf("Good Morning");
+// }
+
+// int main() {
+
+//   greet();
+//   return 0;
+// }
+
+// >> Good Morning
+
+
+
+// #include <stdio.h>
+
+// void greet() {
+//   printf("Good Morning\n");
+// }
+
+// int main() {
+
+//   greet();
+//   greet();
+//   greet();
+//   greet();
+//   printf("After Function Call");
+//   return 0;
+// }
+// >> Good Moring 
+// >> Good Moring 
+// >> Good Moring 
+// >> Good Moring 
+// >> After Function Call 
+
+
+// -------------------
+
+      // -- Function Parameters 
+
+// #include <stdio.h>
+
+// void calculateSquare(int number) {
+//   int square = number * number;
+//   printf("Square of %d is %d\n", number, square);
+// }
+
+// int main() {
+
+//   calculateSquare(5);
+//   return 0;
+// }
+
+// ----------
+
+
+// #include <stdio.h>
+
+// void addNumbers(int number1, int number2) {
+//   int sum = number1 + number2;
+//   printf("Sum of %d and %d is %d", number1, number2, sum);
+// }
+
+// int main() {
+
+//   addNumbers(10, 12);
+//   return 0;
+// }
+
+// >> Sum of 10 and 12 is 22
+
+// ------------------------------------
+
+
+    // -- Return Types 
+
+// #include <stdio.h>
+
+// int addNumbers(int number1, int number2) {
+//   int sum = number1 + number2;
+
+//   return sum;
+// }
+
+// int main() {
+
+//     int result = addNumbers(10, 12);
+//     printf("Result = %d", result);
+
+//   return 0;
+// }
+    
+// >> Result = 22
+
+// ---------------------------------------------
+
+
+    // -- Function Prototype 
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
