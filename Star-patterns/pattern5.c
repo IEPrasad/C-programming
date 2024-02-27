b7fa703e6840de631e457451a3ba64ad2821823f
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-28 03:57:04 +0530

    Add files via upload

---
+++ Star-patterns/pattern5.c
@@ -0,0 +1,39 @@
+/*
+5. Write a program in C to make such a pattern like 
+a pyramid with numbers increased by 1.
+
+   1
+  2 3
+ 4 5 6
+7 8 9 10
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  int i, j, k=1, row;
+  printf("Enter row number less than 10: ");
+  scanf("%d", &row);
+
+  if (row < 10) {
+    for (i = 1; i <= row; i++) {
+      // print leading space 
+      for (j = i; j <= row; j++) {
+        printf(" ");
+      }
+      for (j = 1; j <= row; j++) {
+        if (j <= i) {
+          printf("%d ", k++);
+        } else {
+          printf(" ");
+        }
+      }
+      printf("\n");
+    }
+  }
+
+
+  return 0;
+}
