e2ef140c5b4fb95c5a2891aebb4c68d17390b0ac
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-02-28 23:56:04 +0530

    Add files via upload

---
+++ 03-practice.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+
+  int i, j, row;
+  printf("Enter row number less than 10: ");
+  scanf("%d", &row);
+
+  if (row < 10) {
+    for (i = 1; i <= row; i++) {
+      for (j = i; j < row; j++){
+        printf(" ");
+      }
+      for (j = 1; j <= row; j++) {
+        if (j <= i) {
+          printf("* ");
+        } else {
+          printf(" ");
+        }
+      }
+      printf("\n");
+    }
+
+  } else {
+    printf("Enter row number less than 10");
+  }
+
+  return 0;
+}
+
+/*
+when we enter row number as 6 the out put is
+
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+* * * * * *
+
+*/
