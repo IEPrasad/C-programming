984cdb29d254a26ce22d3ec2efd8e4ab5b3d1705
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-03-01 14:53:10 +0530

    Add files via upload

---
+++ Star-patterns/08-practice-print-diamond.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+
+  int i, j, row;
+  printf("Enter row number less than 10: ");
+  scanf("%d", &row);
+ 
+  if (row < 10) {
+     row = row/2 +1;
+    for (i = 1; i <= row; i++) {
+      
+      for (j = i; j < row; j++) {
+        printf(" ");
+      }
+      for (j = 1; j < row*2; j++) {
+        if (j <= (2*i -1)) {
+          printf("*");
+        } else {
+          printf(" ");
+        }
+      }
+      printf("\n");
+    }
+ 
+    for (i = 2; i <= row; i++) {
+      for (j = 1; j < row*2; j++) {
+        if (j >= (2*i -1)) {
+          printf("*");
+        } else if (j >= i) {
+          printf(" ");
+        }
+      }
+      printf("\n");
+    }
+  } else {
+    printf("Enter row number less than 10");
+  }
+
+  return 0;
+}
