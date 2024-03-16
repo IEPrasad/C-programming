aa32c72c5cb8d22acc98d511b1fbeec3b3484ca4
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-03-16 23:54:12 +0530

    Update 19-arrays.c

--- 19-arrays.c
+++ 19-arrays.c
@@ -30,7 +30,7 @@ int main() {
 
 // Assign Values using index number
 
-
+/*
 #include <stdio.h>
 
 int main() {
@@ -49,6 +49,75 @@ int main() {
   printf("%d ", age[3]);
   printf("%d ", age[4]);
 
+
+  return 0;
+}
+*/
+
+// ------- day 2 -------
+
+// Change array Elements
+
+// #include <stdio.h>
+
+// int main () {
+
+//   int age[5] = {21, 29, 25, 17, 34};
+//   age[2] = 26;
+
+//   printf("%d", age[2]);
+
+
+//   return 0;
+// }
+
+// Loop and Array -------
+
+// #include <stdio.h>
+
+// int main() {
+
+
+// int age[5];
+
+// printf("Enter 5 input values: \n");
+
+// scanf("%d", &age[0]);
+// scanf("%d", &age[1]);
+// scanf("%d", &age[2]);
+// scanf("%d", &age[3]);
+// scanf("%d", &age[4]);
+
+// printf("%d", age[0]);
+// printf("%d", age[1]);
+// printf("%d", age[2]);
+// printf("%d", age[3]);
+// printf("%d", age[4]);
+
+
+//   return 0;
+// }
+
+
+// ------------^^ instead of this code we can use vv this code 
+
+#include <stdio.h>
+
+int main() {
+
+
+int age[5];
+
+printf("Enter 5 input values: \n");
+
+  for (int i = 0; i < 5; ++i) {
+    scanf("%d", &age[i]);
+  }
+
+  for (int i = 0; i < 5; ++i) {
+    printf("%d", age[i]);
+  }
+  
 
   return 0;
 }
