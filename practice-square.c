641fa331d1d9834ed761d92fd54ef5b28cd25b5c
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-05-02 23:06:19 +0530

    Create practice-square.c
    may 02-practice 1

---
+++ practice-square.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+
+    printf("Enter the size of the square box: ");
+    scanf("%d", &size);
+
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            if(i == 0 || i == size - 1 || j == 0 || j == size - 1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
