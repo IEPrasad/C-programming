8f4bb87dcdcc375bacc68c8631fabf63ddd935be
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-06-01 23:05:35 +0530

    Update 09-if-else-statement.c

--- Programize-C/09-if-else-statement.c
+++ Programize-C/09-if-else-statement.c
@@ -132,6 +132,40 @@ int main() {
 
 
 
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    int i, j, row;
+    printf("Enter row number less than 10: ");
+    scanf("%d", &row);
+    
+    if (row < 10) {
+        for (i = 1; i <= row; i++) {
+            for (j = i; j <= row; j++) {
+                printf(" ");
+            }
+            for (j = 1; j < row*2; j++) {
+                if (j <= (2*i -1)) {
+                    printf("*");
+                } else if (j >= i) {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }   
+    } else {
+        printf("Enter row number less than 10");
+    }
+    return 0;
+}
+/* how to create a zip file in linkux lite
+	zip -r lab_test_2.zip 'Lab test 02;
+
+*/
+
+
 
 
 
