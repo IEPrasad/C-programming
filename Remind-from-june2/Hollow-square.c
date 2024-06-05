01073e090b924574f67420e64cb063fb99cce010
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-06-05 23:24:13 +0530

    Update Hollow-square.c

--- Remind-from-june2/Hollow-square.c
+++ Remind-from-june2/Hollow-square.c
@@ -142,5 +142,53 @@ int main() {
 
 */
 
+// ------------------------------------------------------------
+
+
+#include <stdio.h>
+
+int main () {
+
+  int i, j, row;
+  printf("Enter row number less than 10: ");
+  scanf("%d", &row);
+
+  if (row <= 10 && row >= 1) {
+    for (i = 1; i <= row; i++) {
+      for (j = 1; j <= row; j++) {
+        if (j == row || i == 1) {
+          printf("*");
+        } else {
+          printf(" ");
+        }
+        
+      }
+      printf("\n");
+    }
+  }
+
+
+  return 0;
+}
+
+
+// The out put of this code 
+
+/*
+
+**********
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+
+*/
+
+
 
 
