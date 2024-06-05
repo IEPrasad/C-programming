accf3980644d1fc3ecc8b043373073ccf8d6f5b1
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-06-05 22:24:11 +0530

    Update Hollow-square.c

--- Remind-from-june2/Hollow-square.c
+++ Remind-from-june2/Hollow-square.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 
 int main() {
@@ -26,3 +27,52 @@ int main() {
 
   return 0;
 }
+
+// This is not correct code ^^^
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  int i, j, row;
+  printf("Enter row number less than 10: ");
+  scanf("%d", &row);
+
+  if (row >= 1 && row <= 10) {
+    for (i = 1; i <= row; i++) {
+      for (j = 1; j <= row; j++) {
+        if (j == 1 || i == 1 || j == row || i == row || j == i) {
+          printf("*");
+        } else {
+          printf(" ");
+        }
+      }
+        printf("\n");
+
+    }
+
+  } else {
+    printf("Enter row number less than.");
+  }
+
+  return 0;
+}
+
+
+// This is good code 
+// but this is not perfect or or not perfect hollow square
+
+
+
+
+
+
+
+
+
+
+
+
+
