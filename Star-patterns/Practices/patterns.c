55f3516eecc9befe61397dae3450d27e379f4dac
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-06-26 23:44:09 +0530

    Update patterns.c

--- Star-patterns/Practices/patterns.c
+++ Star-patterns/Practices/patterns.c
@@ -1,26 +1,53 @@
+  // #include <stdio.h>
+  
+  // int main() {
+  
+  //   int i, j, row, spaces;
+  //   printf("Enter row number less than 10: ");
+  //   scanf("%d", &row);
+  
+  //   if (row <= 10 && row >= 1) {  
+  //     for (i = 1; i <= row; i++) {
+  //       for (j = 1; j <= 2*row; j++) {
+  //         if (j <= 2*i -1) {
+  //           printf("*");
+  //         } else{
+  //           printf("");
+  //         } 
+  //       } spaces = 1;
+  
+  //     for (spaces = 1; spaces < row ; spaces--) {
+  //       printf(" ");
+  //     } 
+         
+  //       printf("\n");
+  //     }
+  
+  
+  //   } else {
+  //     printf("Enter row number less than 10.");
+  //   }
+  
+  //   return 0;
+  // }
+
 #include <stdio.h>
 
 int main() {
 
-  int i, j, row, spaces;
+  int i, j, row;
   printf("Enter row number less than 10: ");
   scanf("%d", &row);
 
-  if (row <= 10 && row >= 1) {  
+  if (row <= 10 && row >= 1) {
     for (i = 1; i <= row; i++) {
-      for (j = 1; j <= 2*row; j++) {
-        if (j <= 2*i -1) {
-          printf("*");
-        } else{
-          printf("");
-        } 
-      } spaces = 1;
-
-    for (spaces = 1; spaces < row ; spaces--) {
-      printf(" ");
-    } 
-       
-      printf("\n");
+      for (j = 1; j < row; j++) {
+        if (j <= row-1) {
+          printf("s");
+        } else {
+          printf("\n");
+        }
+      }     
     }
 
 
@@ -28,5 +55,21 @@ int main() {
     printf("Enter row number less than 10.");
   }
 
+
   return 0;
 }
+
+
+
+// This is not correct code 
+// I couldn't write the correct code yet ...
+
+
+
+
+
+
+
+
+
+
